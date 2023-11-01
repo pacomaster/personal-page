bdda4170b41262370e4da98bb78f914c6a5591db
pacomaster <fco_amezcua@hotmail.com>
2023-10-31 23:10:10 -0600

    Colored timeline

--- src/App.js
+++ src/App.js
@@ -11,10 +11,6 @@ export default function Main() {
     return (
         <div className="main">
             <PersonalInfo name={personal.name} birthday={personal.birthday} phone={personal.phone} email={personal.email} description={personal.description} />
-            <hr class="solid" />
-            <Experience experience={experience}/>
-            <hr class="solid" />
-            <Education education={education}/>
             <hr class="solid"/>
             <Timeline experience={experience} education={education} />
         </div>
@@ -25,8 +21,7 @@ function Menu(){
     return (
         <div className="menu">
             <div><a href="PersonalInfo">Personal Info</a></div>
-            <div><a href="Experience">Experience</a></div>
-            <div><a href="Education">Education</a></div>
+            <div><a href="Timeline">Timeline</a></div>
         </div>
     );
 }
@@ -51,11 +46,13 @@ function Timeline({experience, education}){
     const expStartYears = experience.map(job => (new Date(job.startDate)).getFullYear());
     const expEndYears = experience.map(job => (new Date(job.endDate)).getFullYear());
     const expNames = experience.map(job => job.company);
+    const expColors = experience.map(job => job.color);
     const expMinYear = Math.min(...expStartYears);
 
     const eduStartYears = education.map(school => (new Date(school.startDate)).getFullYear());
     const eduEndYears = education.map(school => (new Date(school.endDate)).getFullYear());
     const eduNames = education.map(school => school.abbreviation);
+    const eduColors = experience.map(school => school.color);
     const eduMinYear = Math.min(...eduStartYears);
 
     let minYear = expMinYear;
@@ -71,10 +68,12 @@ function Timeline({experience, education}){
     let schools = [];
 
     let expCurrent = "";
+    let expColor = "";
     let expIndex = 0;
     let expPadding = 1;
 
     let eduCurrent = "";
+    let eduColor = "";
     let eduIndex = 0;
     let eduPadding = 1;
 
@@ -82,12 +81,14 @@ function Timeline({experience, education}){
         years.push(i);
 
         if(expEndYears[expIndex] == i){
-            jobs.push({name: expCurrent, colspan: expPadding});
+            jobs.push({name: expCurrent, colspan: expPadding, color: expColor});
             expCurrent = "";
+            expColor = "";
             expIndex++;
         }
         if(expStartYears[expIndex] == i){
             expCurrent = expNames[expIndex];
+            expColor = expColors[expIndex];
             expPadding = 0;
         }
         if(expCurrent == ""){
@@ -97,12 +98,14 @@ function Timeline({experience, education}){
         }
 
         if(eduEndYears[eduIndex] == i){
-            schools.push({name: eduCurrent, colspan: eduPadding});
+            schools.push({name: eduCurrent, colspan: eduPadding, color: eduColor});
             eduCurrent = "";
+            eduColor = "";
             eduIndex++;
         }
         if(eduStartYears[eduIndex] == i){
             eduCurrent = eduNames[eduIndex];
+            eduColor = eduColors[eduIndex];
             eduPadding = 0;
         }
         if(eduCurrent == ""){
@@ -112,10 +115,10 @@ function Timeline({experience, education}){
         }
     }
     if(expCurrent != ""){
-        jobs.push({name: expCurrent, colspan: expPadding});
+        jobs.push({name: expCurrent, colspan: expPadding, color: expColor});
     }
     if(eduCurrent != ""){
-        schools.push({name: eduCurrent, colspan: eduPadding});
+        schools.push({name: eduCurrent, colspan: eduPadding, color: eduColor});
     }
 
     const nYears = years.map( year => {
@@ -125,71 +128,37 @@ function Timeline({experience, education}){
     });
 
     const nJobs = jobs.map( job => {
-        return(
-            <td colspan={job.colspan}>{job.name}</td>
-        );
+        if(job.name != ""){
+            return(<td className={job.color} colspan={job.colspan}>{job.name}</td>);
+        } else {
+            return(<td></td>);
+        }
     });
 
     const nSchools = schools.map( school => {
-        return(
-            <td colspan={school.colspan}>{school.name}</td>
-        );
+        if(school.name != ""){
+            return(<td className={school.color} colspan={school.colspan}>{school.name}</td>);
+        } else {
+        return(<td></td>);
+        }
     });
 
     return(
-        <div className="timeline">
-            <table>
-                <tr>
-                    {nJobs}
-                </tr>
-                <tr>
-                    {nYears}
-                </tr>
-                <tr>
-                    {nSchools}
-                </tr>
-            </table>
+        <div>
+            <h2>Timeline</h2>
+            <div className="timeline">
+                <table>
+                    <tr>
+                        {nJobs}
+                    </tr>
+                    <tr>
+                        {nYears}
+                    </tr>
+                    <tr>
+                        {nSchools}
+                    </tr>
+                </table>
+            </div>
         </div>
     );
-}
-
-function Experience({experience}) {
-
-    const jobs = experience.map(job => {
-        return (
-            <li>
-                <div>{job.company}</div>
-                <div>{job.roles}</div>
-                <div>{job.startDate}</div>
-                <div>{job.endDate}</div>
-            </li>
-        );
-    });
-
-    return (
-        <div className="experience-info">
-            <h2>Experience</h2>
-            <ul>{jobs}</ul>
-        </div>
-        );
-}
-
-function Education({education}) {
-
-    const schools = education.map(school => {
-        return (
-            <li>
-                <div>{school.name}</div>
-                <div>{school.degree}</div>
-                <div>{school.startDate}</div>
-                <div>{school.endDate}</div>
-            </li>
-        );
-    });
-    return (
-        <div className="education-info">
-            <h2>Education</h2>
-            <ul>{schools}</ul>
-        </div>
-        );
 }
