ea50c8cc763505ca73dde18b8c4adfb6311b367f
pacomaster <fco_amezcua@hotmail.com>
2023-12-15 17:48:55 -0600

    Summary

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import OptionsMenu from './OptionsMenu/OptionsMenu'
 import MenuArea from './MenuArea/MenuArea'
 import PersonalInfo from './PersonalInfo/PersonalInfo'
+import Summary from './Summary/Summary'
 import Timeline from './Timeline/Timeline'
 import BottomSection from './BottomSection/BottomSection'
 import cvData from './data/francisco.js';
@@ -16,6 +17,8 @@ export default function Main() {
             <OptionsMenu languages={languages}/>
             <MenuArea />
             <PersonalInfo name={personal.name} birthday={personal.birthday} phone={personal.phone} email={personal.email} description={personal.description} />
+            <hr className="solid"/>
+            <Summary summary={personal.description}/>
             <hr className="solid"/>
             <Timeline experience={experience} education={education} />
             <hr className="solid"/>
---
+++ src/Summary/Summary.js
@@ -0,0 +1,9 @@
+export default function Summary({summary}) {
+    return (
+        <div id="summary">
+            <h1>Summary</h1>
+            <div id="description">{summary}</div>
+        </div>
+    );
+
+}
